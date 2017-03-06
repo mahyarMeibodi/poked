3e04984d110630b7de7edd871e4aa1a9f8481c51
mahyar <mahyar@nobody.invalid>
2017-03-06 15:11:46 -0800

    created a pokemon class and pokeCell view class and also created my graphics

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by mahyar on 3/6/17.
+//  Copyright © 2017 mahyar. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex/pokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  pokeCell.swift
+//  Pokedex
+//
+//  Created by mahyar on 3/6/17.
+//  Copyright © 2017 mahyar. All rights reserved.
+//
+
+import UIKit
+
+class pokeCell: UICollectionViewCell {
+    
+}
